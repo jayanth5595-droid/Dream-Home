413665f240d1dc7d068c192d8dd22230a138c632
jayanth5595-droid <jayanth5595@gmail.com>
2026-08-29 19:54:55 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 /* Put your Supabase Project URL and PUBLISHABLE/anon key here. NEVER use service_role. */
-window.DREAM_HOME={url:"https://qjarnhwwnupnutswmhcr.supabase.co/rest/v1/",key:"sb_publishable_S5qjIBMxNq3UwiKNWdxzvQ_-e_rMN3y"};
+window.DREAM_HOME={url:"https://qjarnhwwnupnutswmhcr.supabase.co",key:"sb_publishable_S5qjIBMxNq3UwiKNWdxzvQ_-e_rMN3y"};
